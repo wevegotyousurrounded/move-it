19385b8a570957f01f8fff6a750b1ac13c14a4b1
Avneet <avneetk000v@gmail.com>
2025-03-18 20:22:15 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import WorkoutTracker from "./pages/WorkoutTracker";
+import Goals from "./pages/Goals";
+import Plans from "./pages/Plans";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import "./assets/css/styles.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/workout" element={<WorkoutTracker />} />
+        <Route path="/goals" element={<Goals />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
